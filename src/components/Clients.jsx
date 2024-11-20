d041a098a8d6845ce41aa1a61b788f126d1d4ffa
Mugdho <saha2171515@stud.kuet.ac.bd>
2024-11-20 17:57:03 +0600

    20/11/2024

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,107 +1,127 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function Clients() {
+    useEffect(() => {
+        if (typeof window !== "undefined" && window.jQuery) {
+            // Initialize the client slider
+            window.jQuery("#client_slider").owlCarousel({
+                items: 6, // Show 6 items at a time
+                loop: true, // Loop through the items
+                autoplay: true, // Autoplay
+                autoplayTimeout: 5000, // Autoplay timeout
+                nav: false, // No navigation arrows
+                dots: true, // Show navigation dots
+                margin: 30, // Margin between items
+            });
+        }
+    }, []);
+
     return (
-        <div>
-            <div className="client pt-5">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-lg-8 col-md-8 wow animated fadeInRight">
-                            <div className="pq-section-title pq-style-1">
-                                <span className="pq-section-sub-title">Working with</span>
-                                <h5 className="pq-section-main-title">Brand in Collention</h5>
-                            </div>
+        <div className="client pt-5">
+            <div className="container">
+                <div className="row">
+                    <div className="col-lg-8 col-md-8 wow animated fadeInRight">
+                        <div className="pq-section-title pq-style-1">
+                            <span className="pq-section-sub-title">Working with</span>
+                            <h5 className="pq-section-main-title">Brand in Collection</h5>
                         </div>
+                    </div>
+                    <div className="col-lg-12">
                         <div className="col-lg-12">
-                            <div className="col-lg-12">
-                                <div className="divider pq-right-border pq-45"></div>
-                            </div>
+                            <div className="divider pq-right-border pq-45"></div>
                         </div>
-                        <div className="col-lg-12 wow animated fadeInRight">
-                            <div className="pq-client-box-slider pq-client-style-1 pq-p-45-90">
-                                <div className="owl-carousel owl-loaded owl-drag" data-dots="false" data-nav="false"
-                                     data-desk_num="6"
-                                     data-lap_num="4" data-tab_num="2" data-mob_num="1" data-mob_sm="1"
-                                     data-autoplay="true"
-                                     data-loop="true" data-margin="30">
-                                    <div className="item">
-                                        <div className="pq-client-box pq-client-style-1">
-                                            <a>
-                                                <img src="assets/images/client/1.png" alt="pq-client-img"
-                                                     className="pq-client-img"/>
-                                                <img src="assets/images/client/h1.png" alt="pq-client-img"
-                                                     className="pq-client-hover-img"/>
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <div className="item">
-                                        <div className="pq-client-box pq-client-style-1">
-                                            <a>
-                                                <img src="assets/images/client/2.png" alt="pq-client-img"
-                                                     className="pq-client-img"/>
-                                                <img src="assets/images/client/h2.png" alt="pq-client-img"
-                                                     className="pq-client-hover-img"/>
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <div className="item">
-                                        <div className="pq-client-box pq-client-style-1">
-                                            <a>
-                                                <img src="assets/images/client/3.png" alt="pq-client-img"
-                                                     className="pq-client-img"/>
-                                                <img src="assets/images/client/h3.png" alt="pq-client-img"
-                                                     className="pq-client-hover-img"/>
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <div className="item">
-                                        <div className="pq-client-box pq-client-style-1">
-                                            <a>
-                                                <img src="assets/images/client/4.png" alt="pq-client-img"
-                                                     className="pq-client-img"/>
-                                                <img src="assets/images/client/h4.png" alt="pq-client-img"
-                                                     className="pq-client-hover-img"/>
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <div className="item">
-                                        <div className="pq-client-box pq-client-style-1">
-                                            <a>
-                                                <img src="assets/images/client/5.png" alt="pq-client-img"
-                                                     className="pq-client-img"/>
-                                                <img src="assets/images/client/h5.png" alt="pq-client-img"
-                                                     className="pq-client-hover-img"/>
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <div className="item">
-                                        <div className="pq-client-box pq-client-style-1">
-                                            <a>
-                                                <img src="assets/images/client/6.png" alt="pq-client-img"
-                                                     className="pq-client-img"/>
-                                                <img src="assets/images/client/h6.png" alt="pq-client-img"
-                                                     className="pq-client-hover-img"/>
-                                            </a>
-                                        </div>
-                                    </div>
-                                    <div className="item">
-                                        <div className="pq-client-box pq-client-style-1">
-                                            <a>
-                                                <img src="assets/images/client/7.png" alt="pq-client-img"
-                                                     className="pq-client-img"/>
-                                                <img src="assets/images/client/h7.png" alt="pq-client-img"
-                                                     className="pq-client-hover-img"/>
-                                            </a>
-                                        </div>
-                                    </div>
+                    </div>
+                    <div className="col-lg-12 wow animated fadeInRight">
+                        <div id="client_slider"
+                             className="pq-client-box-slider pq-client-style-1 pq-p-45-90 owl-carousel">
+                            {/* Slider items here */}
+                            <div className="item">
+                                <div className="pq-client-box">
+                                    <a>
+                                        <img src="assets/images/client/1.png" alt="pq-client-img"
+                                             className="pq-client-img"/>
+                                    </a>
                                 </div>
                             </div>
+                            <div className="item">
+                                <div className="pq-client-box">
+                                    <a>
+                                        <img src="assets/images/client/1.png" alt="pq-client-img"
+                                             className="pq-client-img"/>
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="pq-client-box">
+                                    <a>
+                                        <img src="assets/images/client/1.png" alt="pq-client-img"
+                                             className="pq-client-img"/>
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="pq-client-box">
+                                    <a>
+                                        <img src="assets/images/client/1.png" alt="pq-client-img"
+                                             className="pq-client-img"/>
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="pq-client-box">
+                                    <a>
+                                        <img src="assets/images/client/1.png" alt="pq-client-img"
+                                             className="pq-client-img"/>
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="pq-client-box">
+                                    <a>
+                                        <img src="assets/images/client/1.png" alt="pq-client-img"
+                                             className="pq-client-img"/>
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="pq-client-box">
+                                    <a>
+                                        <img src="assets/images/client/1.png" alt="pq-client-img"
+                                             className="pq-client-img"/>
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="pq-client-box">
+                                    <a>
+                                        <img src="assets/images/client/1.png" alt="pq-client-img"
+                                             className="pq-client-img"/>
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="pq-client-box">
+                                    <a>
+                                        <img src="assets/images/client/1.png" alt="pq-client-img"
+                                             className="pq-client-img"/>
+                                    </a>
+                                </div>
+                            </div>
+                            <div className="item">
+                                <div className="pq-client-box">
+                                    <a>
+                                        <img src="assets/images/client/1.png" alt="pq-client-img"
+                                             className="pq-client-img"/>
+                                    </a>
+                                </div>
+                            </div>
+                            {/* Repeat for other client items */}
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Clients;
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,28 +1,37 @@
-import React from "react";
+import React, {useEffect} from "react";
 
 function Testimonials() {
+    useEffect(() => {
+        // Ensure the Owl carousel initializes after the component is rendered
+        if (typeof window !== "undefined" && window.jQuery) {
+            window.jQuery("#testimonialSlider").owlCarousel({
+                items: 1, // Show one testimonial at a time
+                loop: true, // Loop through testimonials
+                autoplay: true, // Enable autoplay
+                autoplayTimeout: 5000, // Set autoplay timeout
+                nav: false, // No navigation arrows
+                dots: true, // Show dots for navigation
+            });
+        }
+    }, []);
+
     return (
         <div>
             <section className="testimonial pq-bg-img-1">
                 <div className="container">
-                    <div className="row align-items-center justify-content-center">
+                    <div className="row">
                         <div className="col-lg-8 col-md-8 wow animated fadeInLeft">
                             <div className="pq-section-title pq-style-1">
                                 <span className="pq-section-sub-title">our testimonial</span>
                                 <h5 className="pq-section-main-title">What Our Clients Say</h5>
                             </div>
                         </div>
-                        <div className="col-lg-4 col-md-4 wow animated fadeInLeft">
-                        </div>
                         <div className="col-lg-12">
                             <div className="divider pq-left-border pq-45"></div>
                         </div>
                         <div className="col-lg-12 wow animated fadeInUp">
                             <div className="pq-testimonial-box-slider pq-style-2">
-                                <div className="owl-carousel owl-loaded owl-drag" data-dots="true" data-nav="false"
-                                     data-desk_num="1" data-lap_num="1"
-                                     data-tab_num="1" data-mob_num="1" data-mob_sm="1" data-autoplay="true"
-                                     data-loop="true" data-margin="30">
+                                <div className="owl-carousel owl-loaded owl-drag" id="testimonialSlider">
                                     <div className="item">
                                         <div className="pq-testimonial-box pq-style-2 text-left">
                                             <div className="pq-testimonial-media">
@@ -33,19 +42,18 @@ function Testimonials() {
                                             </div>
                                             <div className="pq-testimonial-info">
                                                 <div className="pq-testimonial-content">
-                                                    <p className="pq-testimonial-description">There are many variations
-                                                        of passages of Lorem Ipsum
-                                                        available, the majority have suffered alter ation in someform,
-                                                        by injected humour, or
-                                                        randomised words which don’t look even variations of able. Lorem
-                                                        Ipsum available, the
-                                                        majority have sufferedThere are many variations of passages of
-                                                        It is a long established fact
-                                                        that a reader.</p>
+                                                    <p className="pq-testimonial-description">
+                                                        There are many variations of passages of Lorem Ipsum available,
+                                                        the majority have suffered alter ation in someform, by injected
+                                                        humour, or randomised words which don’t look even variations of
+                                                        able. Lorem Ipsum available, the majority have sufferedThere are
+                                                        many variations of passages of It is a long established fact
+                                                        that a reader.
+                                                    </p>
                                                 </div>
                                                 <div className="pq-team-content-info">
                                                     <div className="pq-testimonial-meta">
-                                                        <h5 className="pq-testmonial-title">Alex john Martin</h5>
+                                                        <h5 className="pq-testmonial-title">Alex John Martin</h5>
                                                         <span>Manager</span>
                                                     </div>
                                                     <div className="pq-testimonial-icon">
@@ -55,6 +63,7 @@ function Testimonials() {
                                             </div>
                                         </div>
                                     </div>
+
                                     <div className="item">
                                         <div className="pq-testimonial-box pq-style-2 text-left">
                                             <div className="pq-testimonial-media">
@@ -65,83 +74,18 @@ function Testimonials() {
                                             </div>
                                             <div className="pq-testimonial-info">
                                                 <div className="pq-testimonial-content">
-                                                    <p className="pq-testimonial-description">There are many variations
-                                                        of passages of Lorem Ipsum
-                                                        available, the majority have suffered alter ation in someform,
-                                                        by injected humour, or
-                                                        randomised words which don’t look even variations of able. Lorem
-                                                        Ipsum available, the
-                                                        majority have sufferedThere are many variations of passages of
-                                                        It is a long established fact
-                                                        that a reader.</p>
+                                                    <p className="pq-testimonial-description">
+                                                        There are many variations of passages of Lorem Ipsum available,
+                                                        the majority have suffered alter ation in someform, by injected
+                                                        humour, or randomised words which don’t look even variations of
+                                                        able. Lorem Ipsum available, the majority have sufferedThere are
+                                                        many variations of passages of It is a long established fact
+                                                        that a reader.
+                                                    </p>
                                                 </div>
                                                 <div className="pq-team-content-info">
                                                     <div className="pq-testimonial-meta">
-                                                        <h5 className="pq-testmonial-title">Alex john Martin</h5>
-                                                        <span>Manager</span>
-                                                    </div>
-                                                    <div className="pq-testimonial-icon">
-                                                        <i className="fas fa-quote-right"></i>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="item">
-                                        <div className="pq-testimonial-box pq-style-2 text-left">
-                                            <div className="pq-testimonial-media">
-                                                <div className="pq-testimonial-img">
-                                                    <img decoding="async" src="assets/images/testimonial/3.jpg"
-                                                         alt="Testimonial-image"/>
-                                                </div>
-                                            </div>
-                                            <div className="pq-testimonial-info">
-                                                <div className="pq-testimonial-content">
-                                                    <p className="pq-testimonial-description">There are many variations
-                                                        of passages of Lorem Ipsum
-                                                        available, the majority have suffered alter ation in someform,
-                                                        by injected humour, or
-                                                        randomised words which don’t look even variations of able. Lorem
-                                                        Ipsum available, the
-                                                        majority have sufferedThere are many variations of passages of
-                                                        It is a long established fact
-                                                        that a reader.</p>
-                                                </div>
-                                                <div className="pq-team-content-info">
-                                                    <div className="pq-testimonial-meta">
-                                                        <h5 className="pq-testmonial-title">Alex john Martin</h5>
-                                                        <span>Manager</span>
-                                                    </div>
-                                                    <div className="pq-testimonial-icon">
-                                                        <i className="fas fa-quote-right"></i>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="item">
-                                        <div className="pq-testimonial-box pq-style-2 text-left">
-                                            <div className="pq-testimonial-media">
-                                                <div className="pq-testimonial-img">
-                                                    <img decoding="async" src="assets/images/testimonial/4.jpg"
-                                                         alt="Testimonial-image"/>
-                                                </div>
-                                            </div>
-                                            <div className="pq-testimonial-info">
-                                                <div className="pq-testimonial-content">
-                                                    <p className="pq-testimonial-description">There are many variations
-                                                        of passages of Lorem Ipsum
-                                                        available, the majority have suffered alter ation in someform,
-                                                        by injected humour, or
-                                                        randomised words which don’t look even variations of able. Lorem
-                                                        Ipsum available, the
-                                                        majority have sufferedThere are many variations of passages of
-                                                        It is a long established fact
-                                                        that a reader.</p>
-                                                </div>
-                                                <div className="pq-team-content-info">
-                                                    <div className="pq-testimonial-meta">
-                                                        <h5 className="pq-testmonial-title">Alex john Martin</h5>
+                                                        <h5 className="pq-testmonial-title">Alex John Martin</h5>
                                                         <span>Manager</span>
                                                     </div>
                                                     <div className="pq-testimonial-icon">
@@ -158,7 +102,7 @@ function Testimonials() {
                 </div>
             </section>
         </div>
-    )
+    );
 }
 
 export default Testimonials;
